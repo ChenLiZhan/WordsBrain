31f0e465474522d9ae729f8672358d22381dbebe
chenlizhan <chung1350@hotmail.com>
2014-10-09 16:17:58 +0800

    Create model word.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra/base'
 require 'haml'
 require 'sinatra/flash'
+require 'data_mapper'
+require './words.rb'
 
 class WordsBrainApp < Sinatra::Base
+  helpers WordHelpers
 
+  configure :development do
+    DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  end
+
+  get '/' do
+
+  end
 end
