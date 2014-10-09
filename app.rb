d3628f92630ae3debc577ae07294922d7b0d662b
chenlizhan <chung1350@hotmail.com>
2014-10-09 17:33:51 +0800

    Move the location of model

--- app.rb
+++ app.rb
@@ -2,16 +2,44 @@ require 'sinatra/base'
 require 'haml'
 require 'sinatra/flash'
 require 'data_mapper'
-require './words.rb'
+require 'dm-core'
+require 'dm-migrations'
+
+class Words
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :word, String
+  property :created_time, DateTime
+end
+
+DataMapper.finalize
+
+module Wordshelpers
+  def create_word(word)
+    Words.count(:word => "#{params[:word]}") == 0 ? Words.create(:word => params[:word], :created_time => Time.now) : false
+  end
+end
 
 class WordsBrainApp < Sinatra::Base
-  helpers WordHelpers
+  helpers Wordshelpers
+  enable :method_override
+
+  configure do
+    enable :sessions
+  end
 
   configure :development do
     DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
   end
 
   get '/' do
+    @words = Words.all
+    haml :index
+  end
 
+  post '/' do
+    create_word
+    redirect to('/')
   end
 end
